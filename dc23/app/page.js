7121146f88a4b9e9c518d7eee6cb57be27b9e943
Lakshay <lakshayyadav2006@gmail.com>
2023-08-13 14:53:13 +0530

    Created the about section.

--- dc23/app/page.js
+++ dc23/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Landing from "@/app/pages/Landing";
 import Introduction from "@/app/pages/Introduction";
+import About from "@/app/pages/About";
 
 export default function Home() {
     return (
         <>
             <Landing/>
             <Introduction />
+            <About />
         </>
     );
 }
---
+++ dc23/app/pages/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "../../styles/About.module.css";
+import Image from "next/image";
+import tesla_background from "../../public/images/tesla_background_upscaled.jpg";
+
+const About = () => {
+    return (
+        <div>
+            <div className={styles.overlay}>
+                <Image src={tesla_background} alt="tesla_background" className={styles.bgImage}/>
+            </div>
+
+            <div className={styles.container}>
+                <h1 className={styles.title}>ABOUT</h1>
+
+                <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non
+                    </p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla
+                    </p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
