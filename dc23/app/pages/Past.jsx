c73b19f8598c7afe529d2d2c45075a47bb84d47a
Lakshay <lakshayyadav2006@gmail.com>
2023-08-19 02:34:36 +0530

    .

--- dc23/app/pages/Past.jsx
+++ dc23/app/pages/Past.jsx
@@ -15,7 +15,7 @@ const TimelineItem = ({image, text}) => {
         <motion.div initial={{opacity: 0}} whileInView={{opacity:1}} viewport={{ once: true }} className={styles.timelineItem}>
             <Image src={image} alt={"tesla timeline image"} className={styles.timelineImage}/>
             <hr />
-            <p> lorem </p>
+            <p className={styles.timelineText}> {text} </p>
         </motion.div>
     );
 };
