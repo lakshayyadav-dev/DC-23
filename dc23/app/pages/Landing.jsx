3ab436153ff3f99265d6c4fecf48e00b45c82173
Lakshay <lakshayyadav2006@gmail.com>
2023-08-19 02:56:20 +0530

    .

--- dc23/app/pages/Landing.jsx
+++ dc23/app/pages/Landing.jsx
@@ -13,19 +13,19 @@ const Landing = () => {
             <Image src={coverImage} alt={"coverImage"} className={styles.coverImage} priority />
 
             <div className={styles.content}>
-                <motion.nav initial={{x:-500}} animate={{x:0}} transition={{ duration: 0.75}} className={styles.navbar}>
+                <nav initial={{x:-500}} animate={{x:0}} transition={{ duration: 0.75}} className={styles.navbar}>
                     <ul>
                         <li><a href={"#about"}>about</a></li>
                         <li><a href={"#past"}>past</a></li>
                         <li><a href={"#inventions"}>inventions</a></li>
                         <li><a href={"#conclusion"}>significance</a></li>
                     </ul>
-                </motion.nav>
+                </nav>
 
-                <motion.div initial={{x:-750}} animate={{x:0}} transition={{ duration: 0.75 }} className={styles.heading}>
+                <div initial={{x:-750}} animate={{x:0}} transition={{ duration: 0.75 }} className={styles.heading}>
                     <h1>NIKOLA TESLA</h1>
                     <p>THE MAN WHO PREDICTED THE FUTURE</p>
-                </motion.div>
+                </div>
             </div>
         </div>
     );
