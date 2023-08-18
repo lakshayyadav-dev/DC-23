bbde343cf9abc402e5005e45398898affb23342c
Lakshay <lakshayyadav2006@gmail.com>
2023-08-18 20:13:54 +0530

    made some final changes.

--- dc23/app/page.js
+++ dc23/app/page.js
@@ -11,7 +11,7 @@ import Footer from "@/app/pages/Footer";
 export default function Home() {
     return (
         <>
-            <Landing/>
+            <Landing />
             <Introduction />
             <About />
             <Past />
